3ef9fb7b85043bd7c15bdf069914bf5b9b96aae3
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-01-31 05:13:25 +0100

    Update ChunkListener.java

--- src/main/java/com/agaloth/spawnershutdown/listener/ChunkListener.java
+++ src/main/java/com/agaloth/spawnershutdown/listener/ChunkListener.java
@@ -1,10 +1,6 @@
 package com.agaloth.spawnershutdown.listener;
 
 import com.agaloth.spawnershutdown.SpawnerShutdown;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
@@ -14,15 +10,12 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Objects;
